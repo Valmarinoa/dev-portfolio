a242cf71c8e03a7dbcd8be636ac22de5234b4a16
Valentina Marino <valenmarinocol@gmail.com>
2023-03-11 10:06:41 -0300

    Implementing About section props

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import Image from "next/image";
 import aboutpic from "../../public/pp.png";
 import { motion } from "framer-motion";
+import { HomeInfo } from "../../typings";
 
-type Props = {};
+type Props = { aboutInfo: HomeInfo };
 
-const About = (props: Props) => {
+const About = ({ aboutInfo }: Props) => {
   return (
     <div className="flex relative flex-col text-center md:text-left h-screen max-w-7xl px-10 justify-evenly mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-2xl text-[#F1FAEE] ">
@@ -31,14 +32,9 @@ const About = (props: Props) => {
           transition={{ duration: 1.2 }}
           whileInView={{ x: 0, opacity: 1 }}
           viewport={{ once: true }}
-          className="text-center text-base max-w-[500px]"
+          className="text-center text-base max-w-[500px] mt-4"
         >
-          <h3>
-            I'm a Front-End Developer who loves to craft sensible digital
-            interfaces. My main focus is on Web Development, but am a designer
-            at heart. <br /> Currently living in Amsterdam, and looking to build
-            cool remote projects around the world!
-          </h3>
+          <h3>{aboutInfo.backgroundInfromation}</h3>
         </motion.div>
       </motion.div>
     </div>
