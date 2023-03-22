3e4786a8e10e9d6fdd8f50976185685461af9e74
Valentina Marino <valenmarinocol@gmail.com>
2023-03-22 13:36:33 +0100

    Implemented new design for experience cards
    Mobile and desktop

--- components/experience/ExperienceComponent.tsx
+++ components/experience/ExperienceComponent.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import React from "react";
 import { Experience } from "../../typings";
 import ExperienceCard from "./experienceCard/ExperienceCard";
@@ -9,13 +10,26 @@ type Props = {
 const ExperienceComponent = ({ experiences }: Props) => {
   console.log(experiences);
   return (
-    <div className="h-screen flex relative overflow-hidden flex-col md-flex-row max-w-full px-10 justify-evenly mx-auto items-center text-left">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-[#F1FAEE] text-2xl">
+    <div className="h-screen flex relative overflow-y-hidden overflow-x-visible flex-col md-flex-row max-w-full px-6 md:w-[70%] justify-evenly mx-auto items-center text-left">
+      <h3 className="absolute top-20 uppercase tracking-[10px] md:tracking-[20px] text-[#F1FAEE] text-2xl">
         Experience
       </h3>
-      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory">
+      {/* <div className="w-full flex flex-col space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory">
         {experiences.map((single) => {
           return <ExperienceCard experience={single} key={single._id} />;
+        })}
+      </div> */}
+      <div className="w-full flex flex-col mt-10 h-[70%] overflow-y-scroll">
+        {experiences.map((single, i) => {
+          return (
+            <motion.div
+              initial={{ y: -30, opacity: 0 }}
+              transition={{ duration: 0.5, delay: i * 0.1 }}
+              whileInView={{ opacity: 1, y: 0 }}
+            >
+              <ExperienceCard experience={single} key={single._id} />
+            </motion.div>
+          );
         })}
       </div>
     </div>
--- components/experience/experienceCard/ExperienceCard.tsx
+++ components/experience/experienceCard/ExperienceCard.tsx
@@ -1,36 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { Experience } from "../../../typings";
 import { urlFor } from "../../../sanity";
 
 type Props = { experience: Experience };
 
 const ExperienceCard = ({ experience }: Props) => {
+  const [open, setOpen] = useState(false);
+  const toggleInfo = () => setOpen(!open);
+
+  console.log(open);
+
   return (
-    <div>
-      <article className="flex flex-col rounded-lg space-y-7 items-center cursor-pointer opacity-100 overflow-hidden md:opacity-40 md:hover:opacity-100 p-4 snap-center border border-white duration-200 flex-shrink-0 w-[300px] md:w-[400px]">
-        <div>
-          <div className="h-32 w-32 mx-auto mb-4 rounded-full overflow-hidden z-10 relative">
-            <img
-              src={urlFor(experience.companyImage).url()}
-              alt="Autofill Tech"
-              className="relative object-cover"
-            />
-          </div>
-          <div className="px-2 md:px-4">
-            <h4 className="text-2xl font-light">{experience.jobTitle}</h4>
-            <p className="font-bold text-2xl mt-1">{experience.company}</p>
-            <p className=" text-sm mt-1">{experience.nature}</p>
-            <p className="uppercase py-5 text-xs">
-              {new Date(experience.dateStarted).toDateString()} -{" "}
-              {experience.isCurrentlyWorkingHere
-                ? "Present"
-                : new Date(experience.dateEnded).toDateString()}
-            </p>
-            <p className="text-xs md:text-sm">{""}</p>
-          </div>
+    <div
+      className="w-full flex border-b border-spacing-1 py-6 text-left cursor-pointer"
+      onClick={toggleInfo}
+    >
+      <div className="w-16 h-16 md:h-20 md:w-20 mx-auto rounded-full relative ">
+        <img
+          src={urlFor(experience.companyImage).url()}
+          alt={experience.company}
+        />
+      </div>
+      <div className="px-2 flex flex-1 md:pl-6">
+        <div className="flex flex-col">
+          <p className="text-xs">
+            {new Date(experience.dateStarted).toDateString()} -{" "}
+            {experience.isCurrentlyWorkingHere
+              ? "Present"
+              : new Date(experience.dateEnded).toDateString()}
+          </p>
+          <h4 className="text-xl md:text-xl">{experience.jobTitle}</h4>
+
+          <span className="flex gap-2 divide-x-[1px] items-center">
+            <p className="font-semibold text-base">{experience.company}</p>
+            <p className=" text-xs font-light pl-2">{experience.nature}</p>
+          </span>
+          {/* Mobile */}
+          <p
+            className={`text-sm pt-2 md:pt-6 md:hidden ${
+              open ? "block" : "hidden"
+            }`}
+          >
+            {experience.role}
+          </p>
         </div>
-      </article>
+      </div>
+      <div className=" md:flex-1 hidden md:flex">
+        <p className="text-xs hidden md:flex">{experience.role}</p>
+      </div>
     </div>
+    // <div>
+    //   <article className="flex flex-col rounded-lg space-y-7 items-center cursor-pointer opacity-100 overflow-hidden md:opacity-40 md:hover:opacity-100 p-4 snap-center border border-white duration-200 flex-shrink-0 w-[300px] md:w-[400px]">
+    //     <div>
+    //       <div className="h-32 w-32 mx-auto mb-4 rounded-full overflow-hidden z-10 relative">
+    //         <img
+    //           src={urlFor(experience.companyImage).url()}
+    //           alt="Autofill Tech"
+    //           className="relative object-cover"
+    //         />
+    //       </div>
+    //       <div className="px-2 md:px-2">
+    //         <h4 className="text-2xl font-light">{experience.jobTitle}</h4>
+    //         <p className="font-bold text-2xl mt-1">{experience.company}</p>
+    //         <p className=" text-sm mt-1">{experience.nature}</p>
+    //         <p className="uppercase py-5 text-xs">
+    //           {new Date(experience.dateStarted).toDateString()} -{" "}
+    //           {experience.isCurrentlyWorkingHere
+    //             ? "Present"
+    //             : new Date(experience.dateEnded).toDateString()}
+    //         </p>
+    //         <p className="text-xs md:text-sm">{""}</p>
+    //       </div>
+    //     </div>
+    //   </article>
+    // </div>
   );
 };
 
