54e2a07c7016d6f0194dcbe0c2a3739e21dfa2e8
Valentina Marino <valenmarinocol@gmail.com>
2023-03-26 20:58:59 +0200

    fixed build by first running dev  then build

--- utils/fetchHomeInfo.ts
+++ utils/fetchHomeInfo.ts
@@ -1,7 +1,7 @@
 import { HomeInfo } from "../typings";
 
 export const fetchHomeInfo = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/getPageInfo`)
 
     const data = await res.json()
     const homeInfo: HomeInfo = data.homeInfo
