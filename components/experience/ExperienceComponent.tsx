6d729077758550064e4a1b7404f25ca6bccd0bd7
Valentina Marino <valenmarinocol@gmail.com>
2023-04-25 12:02:14 +0200

    removed date sorting

--- components/experience/ExperienceComponent.tsx
+++ components/experience/ExperienceComponent.tsx
@@ -8,20 +8,13 @@ type Props = {
 };
 
 const ExperienceComponent = ({ experiences }: Props) => {
-  const sortedByDate = [...experiences].sort(
-    (a, b) => b.dateEnded - a.dateEnded
-  );
-  console.log(sortedByDate);
-  // console.log(experiences.map((i, d) => i.dateStarted));
-  // console.log(experiences.map((ex) => ex.dateStarted));
-
   return (
     <div className="h-screen flex relative flex-col md:flex-row max-w-[1200px] px-6 md:w-[70%] justify-evenly mx-auto items-center text-left">
       <motion.h1
-        initial={{ y: 30, opacity: 0 }}
+        initial={{ opacity: 0, y: 30 }}
         transition={{ duration: 0.5 }}
         whileInView={{ y: 0, opacity: 1 }}
-        viewport={{ once: false }}
+        viewport={{ once: true }}
         className="absolute pl-5 top-24 md:top-20 uppercase tracking-[20px] text-xl text-[#140e2c] mb-6"
       >
         Experience
